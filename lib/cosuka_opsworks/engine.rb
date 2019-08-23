79f11d9032b79fdd33fbc827857ca8d13302ccc2
interu <interu@sonicgarden.jp>
2019-08-23 16:00:16 +0900

    fix: DBがダウンしている場合でもメンテナンス画面が表示されるように

--- lib/cosuka_opsworks/engine.rb
+++ lib/cosuka_opsworks/engine.rb
@@ -8,7 +8,7 @@ module CosukaOpsworks
         middleware = ::Rails.configuration.middleware
         healthcheck_after = ::Rails.configuration.force_ssl ? ActionDispatch::SSL : Rack::Sendfile
         middleware.insert_before healthcheck_after, CosukaOpsworks::Healthcheck
-        middleware.use CosukaOpsworks::Maintenance
+        middleware.insert_before CosukaOpsworks::Healthcheck, CosukaOpsworks::Maintenance
       end
     end
   end
