783208635937ab9c786cad71987ffd530b6ed98f
aki <lala.akira@gmail.com>
2020-04-02 06:26:57 +0900

    refactor: 変数名見直し

--- lib/cosuka_opsworks/tasks.rb
+++ lib/cosuka_opsworks/tasks.rb
@@ -1,11 +1,11 @@
 namespace :cosuka_opsworks do
   desc 'Watch disk space'
-  task :watch_disk_space, [:mount_point, :max_percentage] => :environment do |_, args|
+  task :watch_disk_space, [:mount_point, :threshold] => :environment do |_, args|
     args.with_defaults(mount_point: '/')
-    args.with_defaults(max_percentage: 80)
+    args.with_defaults(threshold: 80)
 
     usage_percentage = CosukaOpsworks::DiskSpace.usage(mount_point: args[:mount_point])
-    if usage_percentage >= args[:max_percentage].to_i
+    if usage_percentage >= args[:threshold].to_i
       raise "[#{CosukaOpsworks::DiskSpace.host_name}] Disk space now using #{usage_percentage}%"
     end
   end
