5b9c9df213fff4ff8cd5b2714804d8a525bebfb5
aki <lala.akira@gmail.com>
2020-04-13 06:57:01 +0900

    refactor: taskの引数見直し

--- lib/cosuka_opsworks/tasks.rb
+++ lib/cosuka_opsworks/tasks.rb
@@ -1,8 +1,7 @@
 namespace :cosuka_opsworks do
   desc 'Watch disk space'
   task :watch_disk_space, [:mount_point, :threshold] => :environment do |_, args|
-    args.with_defaults(mount_point: '/')
-    args.with_defaults(threshold: 80)
+    args.with_defaults(mount_point: '/', threshold: 80)
 
     usage_percentage = CosukaOpsworks::DiskSpace.usage(mount_point: args[:mount_point])
     if usage_percentage >= args[:threshold].to_i
@@ -10,17 +9,17 @@ namespace :cosuka_opsworks do
     end
   end
 
-  desc 'Output cron'
-  task :output_cron, [:rails_env, :master_host] => :environment do |_, args|
+  desc 'Output cron by config/schedule.rb'
+  task :output_cron, [:rails_env, :type] => :environment do |_, args|
     require 'socket'
     require 'whenever'
 
-    args.with_defaults(rails_env: 'production')
-    args.with_defaults(master_host: Socket.gethostname)
+    args.with_defaults(rails_env: 'production', type: 'master')
+    master_host = args[:type] == 'master' ? Socket.gethostname : 'dummy.host'
 
     begin
       original_env = ENV.to_hash
-      ENV.update('RAILS_ENV' => args[:rails_env], 'MASTER_HOST' => args[:master_host])
+      ENV.update('RAILS_ENV' => args[:rails_env], 'MASTER_HOST' => master_host)
       puts Whenever::JobList.new(file: 'config/schedule.rb').generate_cron_output
     ensure
       ENV.replace(original_env)
